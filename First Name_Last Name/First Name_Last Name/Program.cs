c9eb300fd836583b4a66b04bbb7458ddf0029525
Philip Mattsson <maini.ac@hotmail.com>
2018-06-11 22:19:39 +0200

    Version 1

---
+++ First Name_Last Name/First Name_Last Name/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace First_Name_Last_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your First Name ");
+            String First_Name = Console.ReadLine();
+
+            Console.Write("Enter Your Last Name ");
+            String Last_Name = Console.ReadLine();
+
+            Console.WriteLine("Welcome: " + First_Name + " " + Last_Name);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any button to exit");
+            Console.ReadLine();
+
+
+        }
+    }
+}
