c84db4d2916809708d2a5637a3bce34c7e37ea02
Philip Mattsson <maini.ac@hotmail.com>
2018-06-19 18:30:02 +0200

    Version 2

--- First Name_Last Name/First Name_Last Name/Program.cs
+++ First Name_Last Name/First Name_Last Name/Program.cs
@@ -12,7 +12,11 @@ namespace First_Name_Last_Name
             Console.Write("Enter Your Last Name ");
             String Last_Name = Console.ReadLine();
 
-            Console.WriteLine("Welcome: " + First_Name + " " + Last_Name);
+            Console.Write("Enter your year of birth: ");
+            string years = Console.ReadLine();
+            int age = DateTime.Now.Year - int.Parse(years);
+
+            Console.WriteLine("Welcome: " + First_Name + " " + Last_Name + "\nYour current age is: " + age);
 
             Console.WriteLine();
             Console.WriteLine("Press any button to exit");
